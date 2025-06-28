ad12afc907cb87146bc4f97e8bd106a0eed0bb8f
abhaysachan01 <abhaysachan360@gmail.com>
2025-06-28 21:23:44 +0530

    minor change

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 async function connect (){
     try {
         await mongoose.connect(process.env.DB); 
-        console.log("Connect ho gaya");
+        console.log("Connected");
     } catch (error) {
         console.log("Error:", error);
     }
